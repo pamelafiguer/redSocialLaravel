8e3807cd2cfe52559716cfb3462b32d5fa537423
pamelafiguer <pamelaestefani2005@gmail.com>
2024-10-31 23:00:23 -0500

    social

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Web\RedSocialController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(RedSocialController::class) ->group(function() {
+    Route::get('/', 'showLoginForm')->name('login');
+    Route::post('/', 'login')->name('procesar_login');
+
+    Route::get('/registrarse', 'showRegisterForm')->name('registrarse');
+    Route::post('/registrarse', 'register')->name('registrarse');
+
+    Route::get('/recuperarPassword', 'showRecoverForm')->name('recuperarPassword');
+    Route::post('/recuperarPassword', 'recover')->name('procesar_recuperacion');
+
+    Route::get('/cambiarPassword', 'showChangePasswordForm')->name('cambiar_password');
+    Route::post('/cambiarPassword', 'changePassword')->name('actualizar_password');
+
+    Route::get('/bienvenido', 'bienvenido')-> name('ingreso');
+
+    Route::post('/logout', 'logout')-> name('logout');
+    
+    Route::get('/verificar', 'showMesaggeVerificar')-> name('verificar');
+
 });
